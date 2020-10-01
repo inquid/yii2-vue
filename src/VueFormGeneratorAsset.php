d0b5b2488e5809775881c8672eadc3af8312e1d4
Luis Gonzalez <luisarmando1234@gmail.com>
2020-09-30 21:48:58 -0500

    Create VueFormGeneratorAsset.php

---
+++ src/VueFormGeneratorAsset.php
@@ -0,0 +1,17 @@
+
+<?php
+namespace inquid\vue;
+
+/**
+ * Description of VueAsset
+ *
+ * @author Luis Gonzalez <contact@inquid.co>
+ */
+class VueFormGeneratorAsset extends \yii\web\AssetBundle
+{
+    public $baseUrl = 'https://unpkg.com/vue-form-generator/dist/';
+    
+    public $js = [
+        'vfg.js',
+    ];
+}
