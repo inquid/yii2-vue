9d43bdb3c4ad2a279f58cb4fdfc2e5d3ee44d5bb
Luis Gonzalez <luisarmando1234@gmail.com>
2020-10-05 16:31:28 -0500

    Update Vue.php
    Fixing bad merge

--- src/Vue.php
+++ src/Vue.php
@@ -133,7 +133,6 @@ class Vue extends \yii\base\Widget
         $this->view->registerAssetBundle(VueFormGeneratorAsset::className());
     }
 
-<<<<<<< HEAD
     public static function begin($config = array(), BasePluginProvider $pluginProvider = null)
     {
         if ($pluginProvider === null) {
@@ -144,26 +143,16 @@ class Vue extends \yii\base\Widget
         $obj = parent::begin($config);
         echo '<div id="' . $obj->id . '">';
 
-=======
-    public static function begin($config = array()) {
-        $obj =  parent::begin($config);
-        echo '<div id="'.$obj->id.'">';
->>>>>>> 73784c89c3a1883b542ff2b90b7a145538057f54
         return $obj;
     }
 
 
-<<<<<<< HEAD
     public static function end()
     {
-=======
-    public static function end() {
->>>>>>> 73784c89c3a1883b542ff2b90b7a145538057f54
         echo '</div>';
         return parent::end();
     }
 
-<<<<<<< HEAD
     public function run()
     {
         return $this->renderVuejs();
@@ -171,13 +160,6 @@ class Vue extends \yii\base\Widget
 
     public function renderVuejs()
     {
-=======
-    public function run() {
-        return $this->renderVuejs();
-    }
-
-    public function renderVuejs() {
->>>>>>> 73784c89c3a1883b542ff2b90b7a145538057f54
         $data = $this->generateData();
         $methods = $this->generateMethods();
         $filters = $this->generateFilters();
@@ -188,7 +170,6 @@ class Vue extends \yii\base\Widget
         $el = $this->id;
         $js = "
             var {$this->jsName} = new Vue({
-<<<<<<< HEAD
                 el: '#" . $el . "',
                 " . (!empty($this->template) ? "template :'" . $this->template . "'," : null) . "
                 " . (!empty($data) ? "data :" . $data . "," : null) . "
@@ -206,32 +187,12 @@ class Vue extends \yii\base\Widget
                 " . (!empty($this->destroyed) ? "destroyed :" . $this->destroyed->expression . "," : null) . "
                 " . (!empty($this->activated) ? "activated :" . $this->activated->expression . "," : null) . "
                 " . (!empty($this->deactivated) ? "deactivated :" . $this->deactivated->expression . "," : null) . "
-=======
-                el: '#".$el."',
-                ".(!empty($this->template) ? "template :'".$this->template."'," :null)."
-                ".(!empty($data) ? "data :".$data.",":null)."
-                ".(!empty($methods) ? "methods :".$methods."," :null)."
-                ".(!empty($filters) ? "filters :".$filters."," :null)."
-                ".(!empty($watch) ? "watch :".$watch."," :null)."
-                ".(!empty($computed) ? "computed :".$computed."," :null)."
-                ".(!empty($this->beforeCreate) ? "beforeCreate :".$this->beforeCreate->expression."," :null)."
-                ".(!empty($this->created) ? "created :".$this->created->expression."," :null)."
-                ".(!empty($this->beforeMount) ? "beforeMount :".$this->beforeMount->expression."," :null)."
-                ".(!empty($this->mounted) ? "mounted :".$this->mounted->expression."," :null)."
-                ".(!empty($this->beforeUpdate) ? "beforeUpdate :".$this->beforeUpdate->expression."," :null)."
-                ".(!empty($this->updated) ? "updated :".$this->updated->expression."," :null)."
-                ".(!empty($this->beforeDestroy) ? "beforeDestroy :".$this->beforeDestroy->expression."," :null)."
-                ".(!empty($this->destroyed) ? "destroyed :".$this->destroyed->expression."," :null)."
-                ".(!empty($this->activated) ? "activated :".$this->activated->expression."," :null)."
-                ".(!empty($this->deactivated) ? "deactivated :".$this->deactivated->expression."," :null)."
->>>>>>> 73784c89c3a1883b542ff2b90b7a145538057f54
                 components: {
                     \"vue-form-generator\": VueFormGenerator.component
                 },
             }); 
         ";
         Yii::debug($js);
-<<<<<<< HEAD
         Yii::debug('components ' . json_encode($this->generateComponents()));
         Yii::$app->view->registerJs($js, \yii\web\View::POS_END);
     }
@@ -239,26 +200,13 @@ class Vue extends \yii\base\Widget
     public function generateData()
     {
         if (!empty($this->data)) {
-=======
-        Yii::debug('components '.json_encode($this->generateComponents()));
-        Yii::$app->view->registerJs($js, \yii\web\View::POS_END);
-    }
-
-    public function generateData() {
-        if(!empty($this->data)){
->>>>>>> 73784c89c3a1883b542ff2b90b7a145538057f54
             return json_encode($this->data);
         }
     }
 
-<<<<<<< HEAD
     public function generateMethods()
     {
         if (is_array($this->methods) && !empty($this->methods)) {
-=======
-    public function generateMethods() {
-        if(is_array($this->methods) && !empty($this->methods)){
->>>>>>> 73784c89c3a1883b542ff2b90b7a145538057f54
             $str = '';
             foreach ($this->methods as $key => $value) {
                 if ($value instanceof \yii\web\JsExpression) {
@@ -289,23 +237,14 @@ class Vue extends \yii\base\Widget
      */
     public function generateComponents()
     {
-<<<<<<< HEAD
         if (!empty($this->components)) {
-=======
-        if(!empty($this->components)){
->>>>>>> 73784c89c3a1883b542ff2b90b7a145538057f54
             return json_encode($this->components);
         }
     }
 
-<<<<<<< HEAD
     public function generateWatch()
     {
         if (is_array($this->watch) && !empty($this->watch)) {
-=======
-    public function generateWatch() {
-        if(is_array($this->watch) && !empty($this->watch)){
->>>>>>> 73784c89c3a1883b542ff2b90b7a145538057f54
             $str = '';
             foreach ($this->watch as $key => $value) {
                 if ($value instanceof \yii\web\JsExpression) {
@@ -316,14 +255,9 @@ class Vue extends \yii\base\Widget
         }
     }
 
-<<<<<<< HEAD
     public function generateComputed()
     {
         if (is_array($this->computed) && !empty($this->computed)) {
-=======
-    public function generateComputed() {
-        if(is_array($this->computed) && !empty($this->computed)){
->>>>>>> 73784c89c3a1883b542ff2b90b7a145538057f54
             $str = '';
             foreach ($this->computed as $key => $value) {
                 if ($value instanceof \yii\web\JsExpression) {
@@ -334,12 +268,8 @@ class Vue extends \yii\base\Widget
         }
     }
 
-<<<<<<< HEAD
     public function component($tagName, $option)
     {
-=======
-    public function component($tagName, $option) {
->>>>>>> 73784c89c3a1883b542ff2b90b7a145538057f54
         $option = json_encode($option);
         $this->view->registerJs("
             Vue.component($tagName, $option);
